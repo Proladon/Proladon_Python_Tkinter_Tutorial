6cd5f0285905ed6b6833b7dea612e2efa97d7741
Proladon <35193493+Proladon@users.noreply.github.com>
2019-01-24 17:50:57 +0800

    Update tkinter-6.py

--- Code/EP.6/tkinter-6.py
+++ Code/EP.6/tkinter-6.py
@@ -18,9 +18,20 @@ user.grid(row=0, column=0)
 password = Label(text="Password ")
 password.grid(row=1, column=0)
 
-user_entry = Entry(bg="#323232", font="微軟正黑體 20")
-user_entry.grid(row=0, column=1, rowspan=2)
+user_entry = Entry(bg="#323232")
+user_entry.grid(row=0, column=1)
 
+password_entry = Entry(bg="#323232)
+password_entry.grid(row=1, column=1)
 
+# # 跨行Span
+# user = Label(text="User ")
+# user.grid(row=0, column=0)
+
+# password = Label(text="Password ")
+# password.grid(row=1, column=0)
+
+# user_entry = Entry(bg="#323232", font="微軟正黑體 20")
+# user_entry.grid(row=0, column=1, rowspan=2)
 
 win.mainloop()
