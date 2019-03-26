e82920d78857f2a88c3beda82c8f9f783088f3cf
Proladon <Proladon@gmail.com>
2019-03-27 01:24:47 +0800

    Update tkinter-8.py

--- Code/EP.8/tkinter-8.py
+++ Code/EP.8/tkinter-8.py
@@ -1,6 +1,6 @@
 
 #//////////////////////////////////////////////////////////////////////////////#
-#【Proladon】Python GUI - Tkinter EP.7 佈局 pack、place
+#【Proladon】Python GUI - Tkinter EP.8
 # Youtube影片: 
 #//////////////////////////////////////////////////////////////////////////////#
 
@@ -9,6 +9,24 @@ from tkinter import*
 
 
 win = Tk()
-win.geometry("400x200")
+win.geometry("400x400")
+
+def des():
+    fr_1.fr.destroy()
+
+class frame:
+    def __init__(self):
+        self.fr = Frame(win)
+        self.fr.pack(side=LEFT)
+
+fr_1 = frame()
+
+fr_2 = Frame(win)
+fr_2.pack(side=RIGHT)
+
+btn = Button(fr_1, text="Button")
+btn.pack()
+btn = Button(fr_2, text="Button", command= des)
+btn.pack()
 
 win.mainloop()
