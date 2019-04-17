1500c82cd7585df6ed5c7c16f9adfc80fce8e751
Proladon <Proladon@gmail.com>
2019-04-17 18:38:15 +0800

    Update tkinter-8.py

--- Code/EP.8/tkinter-8.py
+++ Code/EP.8/tkinter-8.py
@@ -1,6 +1,6 @@
 
 #//////////////////////////////////////////////////////////////////////////////#
-#【Proladon】Python GUI - Tkinter EP.8
+#【Proladon】Python GUI - Tkinter EP.8 Scale尺度(滑桿)
 # Youtube影片: 
 #//////////////////////////////////////////////////////////////////////////////#
 
@@ -9,23 +9,25 @@ from tkinter import messagebox as Mesg
 
 
 win = Tk()
-win.geometry("400x400")
+win.geometry("400x400+800+300")
 
-ck_value = IntVar()
-ck_value2 = IntVar()
-def show_val():
-    print(ck_value)
-    print(ck_value2)
+def change(self):
+    s_value = s.get() # 讀取拉桿的value
+    win.attributes("-alpha", s_value/100) # 設置視窗透明度
 
-ck = Checkbutton(text="number1", variable=ck_value)
-ck.pack()
+# 方向, 寬度, 長度
+s = Scale(orient=HORIZONTAL, width=100, length=200)
+# 設置範圍 mini 10 ~ max 100
+s.config(from_=10, to=100)
+# 顯示value, 間隔刻度, 解析度, 設置顯示位數
+s.config(showvalue=1, tickinterval=10, resolution=10, digits=1)
+# Label
+s.config(label="Tkinter Slider")
+# 直接設置初始值
+s.set(50)
 
-btn = Button(text="show", command= show_val)
-btn.pack()
-
-
-
-
-
+s.config(command=change)
+s.pack()
 
 win.mainloop()
+
